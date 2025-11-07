760c0bbe8b18e5ef1d9f56de4027b4f2217b9e2f
Ryuqg <skv92571@gmail.com>
2025-11-07 20:22:25 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Относительные пути для GitHub Pages
+  define: {
+    __BASE_PATH__: JSON.stringify('./'),
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `images/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(ext)) {
+            return `css/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    host: true
+  }
+})
